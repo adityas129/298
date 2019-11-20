671a8949457464937bfeace461aa636152c73a7b
stepandr47 <47436247+stepandr47@users.noreply.github.com>
2019-11-19 22:01:38 -0500

    Working Echo with both

--- main.c
+++ main.c
@@ -11,12 +11,30 @@
 
 char ADCState = 0; //Busy state of the ADC
 int16_t ADCResult = 0; //Storage for the ADC conversion result
-int echo_pulse_duration;
-int distance_cm;
+int echo_pulse_duration = 0;
+int distance_cm = 0;
+int distance_cmb = 0;
 
-void main(void)
-{
-    char buttonState = 1; //Current button press state (to allow edge detection)
+int mode = 0;
+int ForB = 0;
+
+int digone = 0;
+int digtwo = 0;
+int digthree = 0;
+int entone = 0;
+int enttwo = 0;
+int entthree = 0;
+int entfour = 0;
+
+int FT_ONE = 200;
+int FT_TWO = 100;
+int FT_THREE = 50;
+int BT_ONE = 100;
+int BT_TWO = 50;
+
+void main(void) {
+    __disable_interrupt();
+    //char buttonState = 1; //Current button press state (to allow edge detection)
 
      /* Functions with two underscores in front are called compiler intrinsics.
      * They are documented in the compiler user guide, not the IDE or MCU guides.
@@ -27,7 +45,6 @@ void main(void)
      * */
 
     //Turn off interrupts during initialization
-    __disable_interrupt();
 
     //Stop watchdog timer unless you plan on using it
     WDT_A_hold(WDT_A_BASE);
@@ -39,6 +56,7 @@ void main(void)
     Init_Clock();   //Sets up the necessary system clocks
     Init_UART();    //Sets up an echo over a COM port
     Init_LCD();     //Sets up the LaunchPad LCD display
+    Init_SW_IRQ();  //Setup push button interrupts
 
      /*
      * The MSP430 MCUs have a variety of low power modes. They can be almost
@@ -50,54 +68,118 @@ void main(void)
      */
     PMM_unlockLPM5(); //Disable the GPIO power-on default high-impedance mode to activate previously configured port settings
 
-    //All done initializations - turn interrupts back on.
     __enable_interrupt();
 
-    displayScrollText("PROJECT 1");
-
-    showChar('L', pos1);
-    showChar('E', pos2);
-    showChar('D', pos3);
-    showChar('S', pos4);
-
-    int x;
-    for (x = 0; x < 4; x++) {
-        GPIO_setOutputHighOnPin(LED_PORT, LED_PIN);
-        _delay_cycles(200000);
-        GPIO_setOutputLowOnPin(LED_PORT, LED_PIN);
-        if (x != 3) {
-            _delay_cycles(200000);
-        }
-    }
-
-    showChar('S', pos1);
-    showChar('O', pos2);
-    showChar('U', pos3);
-    showChar('N', pos4);
-    showChar('D', pos5);
-
-    while(1) //Do this when you want an infinite loop of code
-    {
-        //Buttons SW1 and SW2 are active low (1 until pressed, then 0)
-        if ((GPIO_getInputPinValue(SW2_PORT, SW2_PIN) == 0) & (buttonState == 0)) //Look for rising edge
-        {
-            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
-            buttonState = 1;                //Capture new button state
-            break;
-        }
-        if ((GPIO_getInputPinValue(SW1_PORT, SW1_PIN) == 0) & (buttonState == 1)) //Look for falling edge
-        {
-            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
-            buttonState = 0;                            //Capture new button state
-        }
-        //Start an ADC conversion (if it's not busy) in Single-Channel, Single Conversion Mode
-        /*if (ADCState == 0)
-        {
-            showHex((int)ADCResult); //Put the previous result on the LCD display
-            ADCState = 1; //Set flag to indicate ADC is busy - ADC ISR (interrupt) will clear it
-            ADC_startConversion(ADC_BASE, ADC_SINGLECHANNEL);
-        }*/
-    }
+    Setup:
+//    {
+//        displayScrollText("SETUP MODE");
+//
+//        while (!mode) {
+//            if (entone == 0) {
+//                displayScrollText("SET FT1");
+//            } else if (enttwo == 0) {
+//                showChar(digone+48, pos6);
+//            } else if (entthree == 0) {
+//                showChar(digtwo+48, pos5);
+//            } else if (entfour == 0) {
+//                showChar(digthree+48, pos4);
+//            }
+//        }
+//
+//        FT_ONE = digone + digtwo*10 + digthree*100;
+//
+//        mode = 0;
+//        digone = 0;
+//        digtwo = 0;
+//        digthree = 0;
+//        entone = 0;
+//        enttwo = 0;
+//        entthree = 0;
+//        entfour = 0;
+//
+//        while (!mode) {
+//            if (entone == 0) {
+//                displayScrollText("SET FT2");
+//            } else if (enttwo == 0) {
+//                showChar(digone+48, pos6);
+//            } else if (entthree == 0) {
+//                showChar(digtwo+48, pos5);
+//            } else if (entfour == 0) {
+//                showChar(digthree+48, pos4);
+//            }
+//        }
+//
+//        FT_TWO = digone + digtwo*10 + digthree*100;
+//
+//        mode = 0;
+//        digone = 0;
+//        digtwo = 0;
+//        digthree = 0;
+//        entone = 0;
+//        enttwo = 0;
+//        entthree = 0;
+//        entfour = 0;
+//
+//        while (!mode) {
+//            if (entone == 0) {
+//                displayScrollText("SET FT3");
+//            } else if (enttwo == 0) {
+//                showChar(digone+48, pos6);
+//            } else if (entthree == 0) {
+//                showChar(digtwo+48, pos5);
+//            } else if (entfour == 0) {
+//                showChar(digthree+48, pos4);
+//            }
+//        }
+//
+//        FT_THREE = digone + digtwo*10 + digthree*100;
+//
+//        mode = 0;
+//        digone = 0;
+//        digtwo = 0;
+//        digthree = 0;
+//        entone = 0;
+//        enttwo = 0;
+//        entthree = 0;
+//        entfour = 0;
+//
+//        while (!mode) {
+//            if (entone == 0) {
+//                displayScrollText("SET BT1");
+//            } else if (enttwo == 0) {
+//                showChar(digone+48, pos6);
+//            } else if (entthree == 0) {
+//                showChar(digtwo+48, pos5);
+//            } else if (entfour == 0) {
+//                showChar(digthree+48, pos4);
+//            }
+//        }
+//
+//        BT_ONE = digone + digtwo*10 + digthree*100;
+//
+//        mode = 0;
+//        digone = 0;
+//        digtwo = 0;
+//        digthree = 0;
+//        entone = 0;
+//        enttwo = 0;
+//        entthree = 0;
+//        entfour = 0;
+//
+//        while (!mode) {
+//            if (entone == 0) {
+//                displayScrollText("SET BT2");
+//            } else if (enttwo == 0) {
+//                showChar(digone+48, pos6);
+//            } else if (entthree == 0) {
+//                showChar(digtwo+48, pos5);
+//            } else if (entfour == 0) {
+//                showChar(digthree+48, pos4);
+//            }
+//        }
+//
+//        BT_TWO = digone + digtwo*10 + digthree*100;
+//    }
 
     displayScrollText("SENSOR");
 
@@ -115,20 +197,28 @@ void main(void)
     param3.captureOutputMode = TIMER_A_OUTPUTMODE_OUTBITVALUE;
 
     Timer_A_initContinuousMode (TIMER_A1_BASE, &param2);
-
-    while(1)
-    {
+    mode = 1;
+    while(1) {
+//        displayScrollText("START");
+//        if (!mode) {
+//            displayScrollText("LEAVE");
+//            goto Setup;
+//        }
         Timer_A_clear(TIMER_A1_BASE);
-
-        //send a trigger pulse of 10 us
+//
+//        //send a trigger pulse of 10 us
         GPIO_setOutputHighOnPin(DSO_PORT, DSO_PIN);
-        //Timer_A_outputPWM(TIMER_A0_BASE, &param);
+//        Timer_A_outputPWM(TIMER_A0_BASE, &param);
 
         __delay_cycles(1); //want 10 microsecond
 
         GPIO_setOutputLowOnPin(DSO_PORT, DSO_PIN);
 
+//        showChar('1', pos1);
+
         while(GPIO_getInputPinValue(DSI_PORT, DSI_PIN) == 0){}
+
+        showChar('1', pos2);
 
         Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
 
@@ -142,21 +232,157 @@ void main(void)
 
         char x[15] = {0};
 
+        if (distance_cm < 3) {
+            distance_cm = 3;
+        } else if (distance_cm > 400) {
+            distance_cm = 400;
+        }
+
         sprintf(x, "%d", distance_cm);
 
-//        displayScrollText(x);
-//        showHex((int)distance_cm);
-        showChar(x[0], pos6);
-        showChar(x[1], pos5);
-        showChar(x[2], pos4);
-        showChar(x[3], pos3);
+        if(ForB == 0) {
+            showChar('F', pos1);
+
+            if (distance_cm < 10) {
+                showChar(x[0]+48, pos6);
+            } else if (distance_cm < 100) {
+                showChar(x[0]+48, pos5);
+                showChar(x[1]+48, pos6);
+            } else {
+                showChar(x[0]+48, pos4);
+                showChar(x[1]+48, pos5);
+                showChar(x[2]+48, pos6);
+            }
+        }
+
+        if (distance_cm < FT_THREE) {
+            GPIO_setOutputLowOnPin(GREEN_PORT, GREEN_PIN);
+            GPIO_setOutputLowOnPin(ORANGE_PORT, ORANGE_PIN);
+            GPIO_setOutputLowOnPin(YELLOW_PORT, YELLOW_PIN);
+            GPIO_setOutputHighOnPin(RED_PORT, RED_PIN);
+        } else if (distance_cm < FT_TWO) {
+            GPIO_setOutputLowOnPin(GREEN_PORT, GREEN_PIN);
+            GPIO_setOutputLowOnPin(ORANGE_PORT, ORANGE_PIN);
+            GPIO_setOutputHighOnPin(YELLOW_PORT, YELLOW_PIN);
+            GPIO_setOutputLowOnPin(RED_PORT, RED_PIN);
+        } else if (distance_cm < FT_ONE) {
+            GPIO_setOutputLowOnPin(GREEN_PORT, GREEN_PIN);
+            GPIO_setOutputHighOnPin(ORANGE_PORT, ORANGE_PIN);
+            GPIO_setOutputLowOnPin(YELLOW_PORT, YELLOW_PIN);
+            GPIO_setOutputLowOnPin(RED_PORT, RED_PIN);
+        } else {
+            GPIO_setOutputHighOnPin(GREEN_PORT, GREEN_PIN);
+            GPIO_setOutputLowOnPin(ORANGE_PORT, ORANGE_PIN);
+            GPIO_setOutputLowOnPin(YELLOW_PORT, YELLOW_PIN);
+            GPIO_setOutputLowOnPin(RED_PORT, RED_PIN);
+        }
+        __delay_cycles(170000);
+
+        Timer_A_clear(TIMER_A1_BASE);
+
+        //send a trigger pulse of 10 us
+        GPIO_setOutputHighOnPin(DSOB_PORT, DSOB_PIN);
+
+        __delay_cycles(1); //want 10 microsecond
+
+        GPIO_setOutputLowOnPin(DSOB_PORT, DSOB_PIN);
+
+//        showChar('3', pos3);
+
+        while(GPIO_getInputPinValue(DSIB_PORT, DSIB_PIN) == 0){}
+
+        showChar('2', pos3);
+
+        Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
+
+        while(GPIO_getInputPinValue(DSIB_PORT, DSIB_PIN) == 1){}
+
+        Timer_A_stop(TIMER_A1_BASE);
+
+        echo_pulse_duration = Timer_A_getCounterValue(TIMER_A1_BASE);
+
+        distance_cmb = echo_pulse_duration/58;
+
+        if (distance_cmb < 3) {
+            distance_cmb = 3;
+        } else if (distance_cmb > 400) {
+            distance_cmb = 400;
+        }
+
+        sprintf(x, "%d", distance_cmb);
+
+        if(ForB == 1) {
+            showChar('B', pos1);
+
+            if (distance_cmb < 10) {
+                showChar(x[0]+48, pos6);
+            } else if (distance_cmb < 100) {
+                showChar(x[0]+48, pos5);
+                showChar(x[1]+48, pos6);
+            } else {
+                showChar(x[0]+48, pos4);
+                showChar(x[1]+48, pos5);
+                showChar(x[2]+48, pos6);
+            }
+        }
+
+        if (distance_cmb < BT_TWO) {
+            Init_PWM2(200, 100); //Set period and high count
+
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(100000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+
+            _delay_cycles(100000);
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(100000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+
+            _delay_cycles(100000);
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(100000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+
+            _delay_cycles(100000);
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(100000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+        } else if (distance_cmb < BT_ONE) {
+            Init_PWM2(300, 150); //Set period and high count
+
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(200000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+
+            _delay_cycles(200000);
+            Timer_A_outputPWM(TIMER_A0_BASE, &param);   //Turn on PWM
+            _delay_cycles(200000);
+            Timer_A_stop(TIMER_A0_BASE);    //Shut off PWM signal
+        }
 
         __delay_cycles(170000);
+        showChar('0', pos1);
+        showChar('0', pos2);
+        showChar('0', pos3);
+        showChar('0', pos4);
     }
 }
 
-void Init_GPIO(void)
-{
+void Init_SW_IRQ(void) {
+    //Initialize SW1 Interrupt
+    GPIO_setAsInputPinWithPullUpResistor(SW1_PORT, SW1_PIN);
+    GPIO_enableInterrupt(SW1_PORT, SW1_PIN);
+    GPIO_selectInterruptEdge(SW1_PORT, SW1_PIN, GPIO_LOW_TO_HIGH_TRANSITION);
+    GPIO_clearInterrupt(SW1_PORT, SW1_PIN);
+
+    //Initialize SW2 Interrupt
+    GPIO_setAsInputPinWithPullUpResistor(SW2_PORT, SW2_PIN);
+    GPIO_enableInterrupt(SW2_PORT, SW2_PIN);
+    GPIO_selectInterruptEdge(SW2_PORT, SW2_PIN, GPIO_LOW_TO_HIGH_TRANSITION);
+    GPIO_clearInterrupt(SW2_PORT, SW2_PIN);
+}
+
+void Init_GPIO(void) {
     // Set all GPIO pins to output low to prevent floating input and reduce power consumption
     GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
     GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
@@ -180,21 +406,32 @@ void Init_GPIO(void)
     GPIO_setAsInputPinWithPullUpResistor(SW1_PORT, SW1_PIN);
     GPIO_setAsInputPinWithPullUpResistor(SW2_PORT, SW2_PIN);
 
-    //Set LED1 and LED2 as outputs
-    //GPIO_setAsOutputPin(LED1_PORT, LED1_PIN); //Comment if using UART
-    GPIO_setAsOutputPin(LED2_PORT, LED2_PIN);
+    //Speaker 1
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN7);
 
-    //Set Speaker input and output
-    GPIO_setAsOutputPin(LED_PORT, LED_PIN);
-
-    //Sensor
+    //Sensor 1
     GPIO_setAsInputPin(DSI_PORT, DSI_PIN);
     GPIO_setAsOutputPin(DSO_PORT, DSO_PIN);
+
+    //Sensor 2
+    GPIO_setAsInputPin(DSIB_PORT, DSIB_PIN);
+    GPIO_setAsOutputPin(DSOB_PORT, DSOB_PIN);
+
+    //Green LED
+    GPIO_setAsOutputPin(GREEN_PORT, GREEN_PIN);
+
+    //Orange LED
+    GPIO_setAsOutputPin(ORANGE_PORT, ORANGE_PIN);
+
+    //Yellow LED
+    GPIO_setAsOutputPin(YELLOW_PORT, YELLOW_PIN);
+
+    //Red LED
+    GPIO_setAsOutputPin(RED_PORT, RED_PIN);
 }
 
 /* Clock System Initialization */
-void Init_Clock(void)
-{
+void Init_Clock(void) {
     /*
      * The MSP430 has a number of different on-chip clocks. You can read about it in
      * the section of the Family User Guide regarding the Clock System ('cs.h' in the
@@ -227,8 +464,7 @@ void Init_Clock(void)
 }
 
 /* UART Initialization */
-void Init_UART(void)
-{
+void Init_UART(void) {
     /* UART: It configures P1.0 and P1.1 to be connected internally to the
      * eSCSI module, which is a serial communications module, and places it
      * in UART mode. This let's you communicate with the PC via a software
@@ -262,8 +498,7 @@ void Init_UART(void)
         param.uartMode          = EUSCI_A_UART_MODE;
         param.overSampling      = 1;
 
-    if(STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A0_BASE, &param))
-    {
+    if(STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A0_BASE, &param)) {
         return;
     }
 
@@ -278,21 +513,41 @@ void Init_UART(void)
 /* EUSCI A0 UART ISR - Echoes data back to PC host */
 #pragma vector=USCI_A0_VECTOR
 __interrupt
-void EUSCIA0_ISR(void)
-{
+void EUSCIA0_ISR(void) {
     uint8_t RxStatus = EUSCI_A_UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG);
 
     EUSCI_A_UART_clearInterrupt(EUSCI_A0_BASE, RxStatus);
 
-    if (RxStatus)
-    {
+    if (RxStatus) {
         EUSCI_A_UART_transmitData(EUSCI_A0_BASE, EUSCI_A_UART_receiveData(EUSCI_A0_BASE));
     }
 }
 
+
 /* PWM Initialization */
-void Init_PWM(void)
-{
+void Init_PWM2(int period, int hc) {
+    /*
+     * The internal timers (TIMER_A) can auto-generate a PWM signal without needing to
+     * flip an output bit every cycle in software. The catch is that it limits which
+     * pins you can use to output the signal, whereas manually flipping an output bit
+     * means it can be on any GPIO. This function populates a data structure that tells
+     * the API to use the timer as a hardware-generated PWM source.
+     *
+     */
+    //Generate PWM - Timer runs in Up-Down mode
+    param.clockSource           = TIMER_A_CLOCKSOURCE_SMCLK;
+    param.clockSourceDivider    = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    param.timerPeriod           = period; //Defined in main.h
+    param.compareRegister       = TIMER_A_CAPTURECOMPARE_REGISTER_1;
+    param.compareOutputMode     = TIMER_A_OUTPUTMODE_RESET_SET;
+    param.dutyCycle             = hc; //Defined in main.h
+
+    //PWM_PORT PWM_PIN (defined in main.h) as PWM output
+    GPIO_setAsPeripheralModuleFunctionOutputPin(PWM_PORT, PWM_PIN, GPIO_PRIMARY_MODULE_FUNCTION);
+}
+
+/* PWM Initialization */
+void Init_PWM(void) {
     /*
      * The internal timers (TIMER_A) can auto-generate a PWM signal without needing to
      * flip an output bit every cycle in software. The catch is that it limits which
@@ -313,8 +568,7 @@ void Init_PWM(void)
     GPIO_setAsPeripheralModuleFunctionOutputPin(PWM_PORT, PWM_PIN, GPIO_PRIMARY_MODULE_FUNCTION);
 }
 
-void Init_ADC(void)
-{
+void Init_ADC(void) {
     /*
      * To use the ADC, you need to tell a physical pin to be an analog input instead
      * of a GPIO, then you need to tell the ADC to use that analog input. Defined
@@ -370,8 +624,7 @@ void Init_ADC(void)
 //ADC interrupt service routine
 #pragma vector=ADC_VECTOR
 __interrupt
-void ADC_ISR(void)
-{
+void ADC_ISR(void) {
     uint8_t ADCStatus = ADC_getInterruptStatus(ADC_BASE, ADC_COMPLETED_INTERRUPT_FLAG);
 
     ADC_clearInterrupt(ADC_BASE, ADCStatus);
@@ -381,4 +634,75 @@ void ADC_ISR(void)
         ADCState = 0; //Not busy anymore
         ADCResult = ADC_getResults(ADC_BASE);
     }
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt
+void SW1(void) {
+    __disable_interrupt();
+
+    GPIO_clearInterrupt(SW1_PORT, SW1_PIN);
+
+    if (!mode) {
+        //displayScrollText("SW1");
+        if (enttwo == 0) {
+            digone++;
+            if (digone > 9) {
+                digone = 0;
+            }
+        } else if (entthree == 0) {
+            digtwo++;
+            if (digtwo > 9) {
+                digtwo = 0;
+            }
+        } else if (entfour == 0) {
+            digthree++;
+            if (digthree > 9) {
+                digthree = 0;
+            }
+        }
+    } else {
+        if (ForB == 0) {
+            ForB = 1;
+        } else {
+            ForB = 0;
+        }
+    }
+
+    _delay_cycles(200000);
+    __enable_interrupt();
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt
+void SW2(void) {
+    __disable_interrupt();
+
+    GPIO_clearInterrupt(SW2_PORT, SW2_PIN);
+
+    if (!mode) {
+        //displayScrollText("SW2");
+        if (entone == 0) {
+            entone = 1;
+        } else if (enttwo == 0) {
+            enttwo = 1;
+        } else if (entthree == 0) {
+            entthree = 1;
+        } else if (entfour == 0) {
+            entfour = 1;
+            mode = 1;
+        }
+    } else {
+        mode = 0;
+        digone = 0;
+        digtwo = 0;
+        digthree = 0;
+        entone = 0;
+        enttwo = 0;
+        entthree = 0;
+        entfour = 0;
+    }
+
+    _delay_cycles(200000);
+    __enable_interrupt();
 }
